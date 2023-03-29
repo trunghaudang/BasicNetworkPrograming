a883d912b7e9761fcf36be2aa77a41217d493395
trunghaudang <126368523+trunghaudang@users.noreply.github.com>
2023-03-29 11:33:21 +0700

    Week3 upload

---
+++ Week3/Server/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_ServerView = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_ServerView
+            // 
+            this.txt_ServerView.Location = new System.Drawing.Point(47, 44);
+            this.txt_ServerView.Multiline = true;
+            this.txt_ServerView.Name = "txt_ServerView";
+            this.txt_ServerView.ReadOnly = true;
+            this.txt_ServerView.Size = new System.Drawing.Size(690, 323);
+            this.txt_ServerView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_ServerView);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ServerView;
+    }
+}
+
---
+++ Week3/Server/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+
+        private int Port = 8080; // Port number for the server
+        private readonly UdpClient udpServer; 
+        private Thread listenThread; // Thread for listening message
+        public Form1()
+        {
+            InitializeComponent();            
+            udpServer = new UdpClient(Port);
+
+            // Start the listening thread
+            listenThread = new Thread(ListenForClients);
+            listenThread.Start();
+        }
+
+        void ListenForClients()
+        {
+
+             while (true)
+             {
+                    // Receive message from client
+                    IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] clientData = udpServer.Receive(ref clientEndPoint);
+                    string message = Encoding.Unicode.GetString(clientData);
+
+                    // Show the received message
+                    Invoke(new Action(() =>
+                    {
+                        txt_ServerView.AppendText($"Client ({clientEndPoint.Port}): {message}\r\n");
+                    }));
+
+                    // Send message back to the client
+                    byte[] responseData = Encoding.Unicode.GetBytes(message);
+                    udpServer.Send(responseData, responseData.Length, clientEndPoint);
+
+             }
+             
+        }
+        
+    }
+}
